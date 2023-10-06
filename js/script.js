45e81db404983fe1dc0bda44d82d3463e466c68a
Yakov <lawrinov.yackov@yandex.ru>
2023-10-06 10:55:34 +0300

    edits to the Java script Script animation of falling characters

--- js/script.js
+++ js/script.js
@@ -1,3 +1,8 @@
+const FONT_SIZE = 16;
+const GREEN = "#00d743";
+const SYMBOLS = "A BC DE FG HI JK LM NO PQ RS TU VW XY Z";
+const SYMBOL_DROP_PROBABILITY = 0.98;
+
 class Column {
   constructor(x, fontSize, canvasHeight, context) {
     this.x = x;
@@ -8,12 +13,12 @@ class Column {
   }
 
   drawSymbol() {
-    if (this.y === 0 && Math.random() < 0.98) {
+    if (this.y === 0 && Math.random() < SYMBOL_DROP_PROBABILITY) {
       return;
     }
-    const CHARACTERS = "A BC DE FG HI JK LM NO PQ RS TU VW XY Z";
-    const characterIndex = Math.floor(Math.random() * CHARACTERS.length);
-    const symbol = CHARACTERS[characterIndex];
+
+    const symbolIndex = Math.floor(Math.random() * SYMBOLS.length);
+    const symbol = SYMBOLS[symbolIndex];
 
     this.context.fillText(symbol, this.x, this.y);
 
@@ -31,8 +36,6 @@ const context = canvas.getContext("2d");
 canvas.width = document.documentElement.clientWidth;
 canvas.height = document.documentElement.clientHeight;
 
-const FONT_SIZE = 16;
-const GREEN = "#00d743";
 const columns = [];
 const columnsCount = canvas.width / FONT_SIZE;
 
@@ -40,18 +43,30 @@ for (let i = 0; i < columnsCount; i++) {
   columns.push(new Column(i * FONT_SIZE, FONT_SIZE, canvas.height, context));
 }
 
-context.font = `bold ${FONT_SIZE}px monospace`;
-
-const column = new Column(100, FONT_SIZE, canvas.height, context);
-
 function animate() {
   context.fillStyle = "rgba(0, 0, 0, 0.05)";
   context.fillRect(0, 0, canvas.width, canvas.height);
 
   context.fillStyle = GREEN;
+  context.font = `bold ${FONT_SIZE}px monospace`;
   columns.forEach((column) => column.drawSymbol());
 
   setTimeout(() => requestAnimationFrame(animate), 50);
 }
+
+function resetCanvas() {
+  canvas.width = document.documentElement.clientWidth;
+  canvas.height = document.documentElement.clientHeight;
+
+  columns.length = 0;
+  const columnsCount = canvas.width / FONT_SIZE;
+  for (let i = 0; i < columnsCount; i++) {
+    columns.push(new Column(i * FONT_SIZE, FONT_SIZE, canvas.height, context));
+  }
+
+  context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
+window.addEventListener("resize", resetCanvas);
 
 animate();
